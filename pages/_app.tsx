ef10db6a4a6041a28d4667e5cf7f92f23cf22912
0xArb <arb@subtract.fi>
2022-11-30 22:56:03 +0100

    footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Footer } from "../components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Footer />
       <Toaster />
     </>
   );
